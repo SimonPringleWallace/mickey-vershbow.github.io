0b9ab0d52f8b86dc2a2cfc1e6c71f5e73b4e7529
Mickey Vershbow <mickey.vershbow@gmail.com>
2021-02-27 18:19:10 -0500

    Added Main Dom Elements and App State Variable to JS file. API key no longer working. Reverting back to older commit to debug.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,37 @@
-const BASE_URL = "https://cdn.contentful.com"
-// anything after the ? is called "query strings". Backend developers design what queries will work with the API.
-const API_KEY = "/spaces/1ooy33zp4esg/environments/master/entries?access_token=GwmWVEBSVzK_noU9IhoIaYblT31-CqoiESVSAdo7UJ0&&content_type=triviaq"
+//////////////////////////
+// ! APP STATE
+//////////////////////////
+
+const state = {
+    player1: 0,
+    player2: 0,
+    currentQuestion: {} //empty obect
+}
+
+//////////////////////////
+// ! Main DOM Elements
+//////////////////////////
+
+const question = document.querySelector("#question");
+const a = document.querySelector("#a");
+const b = document.querySelector("#b");
+const c = document.querySelector("#c");
+const d = document.querySelector("#d");
+const e = document.querySelector("#e");
+const p1Score = document.querySelector("#player1 h4");
+const p2Score = document.querySelector("#player2 h4");
+
+console.log(p1Score, p2Score);
+
+//////////////////////////
+// ! Main App Logic
+//////////////////////////
+
+
+
+
+// const BASE_URL = "https://cdn.contentful.com"
+// const API_KEY = "/spaces/1ooy33zp4esg/environments/master/entries?access_token=GwmWVEBSVzK_noU9IhoIaYblT31-CqoiESVSAdo7UJ0&&content_type=triviaq"
 
 const COMPLETE_URL = "https://cdn.contentful.com/spaces/1ooy33zp4esg/environments/master/entries?access_token=GwmWVEBSVzK_noU9IhoIaYblT31-CqoiESVSAdo7UJ0&&content_type=triviaq"
 
