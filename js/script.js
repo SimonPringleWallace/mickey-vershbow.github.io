f39fb7f2e8a20c0840021bde560ed15163a702b5
Mickey Vershbow <mickey.vershbow@gmail.com>
2021-03-04 16:30:07 -0500

    Added a 'correct answer' HTML element that displays the correct answer. Added a 'next question' button although it's not functinal yet. I'd like to make it so that the board doesn't reset until the user clicks the NEXT button

--- js/script.js
+++ js/script.js
@@ -24,8 +24,12 @@ const $p1Score = $("#player1 h4");
 const $p2Score = $("#player2 h4");
 const $correctAnswer = $("#correct-answer")
 
+const container = document.querySelector('.dropdown-menu');
+const restart = container.querySelector('sl-menu-item[value="restart"]');
+const reading = container.querySelector('sl-menu-item[value="reading"]');
+const action = container.querySelector('sl-menu-item[value="action"]');
+const nextQuestion = document.querySelector('#nextQuestion');
 
-console.log($p1Score, $p2Score);
 
 ////////////////////////////
 // ! Functions
@@ -33,27 +37,28 @@ console.log($p1Score, $p2Score);
 
 const chooseAnswer = (event, question) => {
     if (event.target.textContent === question.answer){
-        //! how to make this appear in html?
-        console.log("Correct! The answer is " + question.answer);
+        console.log("Correct! The answer is: " + question.answer);
         highlightCorrectAnswer();
         if (state.which) {
             state.player1++;
             state.which = !state.which;
+            document.getElementById("correct-answer").textContent = "Correct! The answer is: " + question.answer;
         }else{
             state.player2++
+            document.getElementById("correct-answer").textContent = "Correct! The answer is: " + question.answer;
             state.which = !state.which
         }
         setBoard(questions);
     } else {
         console.log("incorrect")
-        highlightIncorrectAnswer();
+        highlightCorrectAnswer();
+        document.getElementById("correct-answer").textContent = "Incorrect. The answer is " + question.answer;
         setBoard(questions);
         state.which = !state.which;
   }
-//! how to display correct answer?
 };
 
-//! how to I setTimeout for this function? Also a fade-out/in for the question and answer text?
+
 const setBoard = (q) => {
     //Getting a random question
     const randomIndex = Math.floor(Math.random() * q.length);
@@ -75,36 +80,67 @@ const setBoard = (q) => {
     $("li").off();
     $("li").on("click", (event) => {
         chooseAnswer(event, randomQuestion)
-        //New Game event listener
-        $("#new-game").off();
-        $("#new-game").on("click", setNewBoard);
     });
 }
 
 // Start a new game
+// const setNewBoard = (q) => {
+//     //Getting a random question
+//     const randomIndex = Math.floor(Math.random() * q.length);
+//     const randomQuestion = q[randomIndex];
 
+//     //Update question
+//     $question.text(randomQuestion.question);
+//     $a.text(randomQuestion.a);
+//     $b.text(randomQuestion.b);
+//     $c.text(randomQuestion.c);
+//     $d.text(randomQuestion.d);
+//     $e.text(randomQuestion.e);
 
+//     //update players scores
+//     $p1Score.text(0);
+//     $p2Score.text(0);
 
+//     // Restart Game event listener; remove the previous event listener before adding a new one so they don't accumulate
+//     $("#restart").off();
+//     $("#restart").on("click", (event) => {
+//         chooseAnswer(event, randomQuestion);
+//         console.log("the game was restarted")
+//     });
+// }
 
-
-
-// turn player buttons red or green depending on whether they got the right answer
-//! how do I reset the color buttons? Include in setBoard function?
 const highlightCorrectAnswer = () => {
     if(state.which){
         document.querySelector("#player1").style.backgroundColor = "green";
-    } if(!state.which){
-        document.querySelector("#player2").style.backgroundColor = "green";
-    }
-}
-
-const highlightIncorrectAnswer = () => {
-    if(state.which){
+    } else {
         document.querySelector("#player1").style.backgroundColor = "red";
-    } if(!state.which){
+    }
+    if(!state.which){
+        document.querySelector("#player2").style.backgroundColor = "green";
+    } else {
         document.querySelector("#player2").style.backgroundColor = "red";
     }
+    //reset button color
 }
+
+// Update player button color
+// const resetButtonColor = () => {
+//     $("#player1").css("background-color", "grey");
+//     $("#player2").css("background-color", "grey");
+// }
+
+
+////////////////////////////////////
+//! Dropdown Menu Event Listeners
+///////////////////////////////////
+
+//! how do I set the event listener to open a new link when the button is clicked?
+restart.addEventListener('click', () => console.log('game restarted'));
+reading.addEventListener('click', () => console.log('extracurricular reading links'));
+action.addEventListener('click', () => console.log('links to things you can do'));
+
+// let $readingLinks = $(`<a id="readingLink" href="http://www.google.com"></a>`)
+// $("#readingLink").attr("target", "_blank");
 
 
 //////////////////////////
